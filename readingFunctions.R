888f3dddb926a588e5eb8161c129cb0c7be83c85
Anika John <anjohn@eu-login-28.euler.ethz.ch>
2024-12-10 08:34:58 +0100

    changes to reading function

--- readingFunctions.R
+++ readingFunctions.R
@@ -23,8 +23,12 @@ read_vcf <- function(file_path){
     
   }else{
   
-  dt[, c('DP', 'AF', 'SB', 'DP4', 'V13', 'V14') := tstrsplit(INFO, ';', fixed = TRUE)]
-  dt[, c('DP4', 'V13', 'V14','INFO') := NULL]
+  #different output format with parallelized lofreq
+
+  #dt[, c('DP', 'AF', 'SB', 'DP4', 'V13', 'V14') := tstrsplit(INFO, ';', fixed = TRUE)]
+  dt[, c('DP', 'AF', 'SB') := tstrsplit(INFO, ';', fixed = TRUE)[1:3]]
+  #dt[, c('DP4', 'V13', 'V14','INFO') := NULL]
+  dt[, c('INFO') := NULL]
   dt[, `:=`(
     AF = str_remove_all(AF, "AF="),
     DP = str_remove_all(DP, "DP="),
@@ -51,7 +55,7 @@ read_vcf <- function(file_path){
 #'
 #' @examples
 #' 
-read_and_mark_vcf <- function(file_path, regex_sample = "\\d{2}_\\d{4}_\\d{2}_\\d{2}"){
+read_and_mark_vcf <- function(file_path, regex_sample = "[A-Z]\\d{1}_\\d{2}_\\d{4}_\\d{2}_\\d{2}"){
   
   dt = read_vcf(file_path)
   
@@ -66,7 +70,7 @@ read_and_mark_vcf <- function(file_path, regex_sample = "\\d{2}_\\d{4}_\\d{2}_\\
     dt[, sample_name := str_extract(file_path,regex_sample)]
     #only care about point mutations
     dt = dt[nchar(REF) == 1 & nchar(ALT) == 1]
-    dt[, c('location_code', 'year','month','day') := tstrsplit(sample_name, "_", fixed=TRUE)]
+    dt[, c('cell','location_code', 'year','month','day') := tstrsplit(sample_name, "_", fixed=TRUE)]
     dt[, c('date') := paste(year,month,day,sep = "-")]
     dt[, c('year','month','day') := NULL]
   
