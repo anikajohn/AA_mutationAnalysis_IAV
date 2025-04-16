b94b2561fcd2f240d0098d3703086f3a091d2889
anikajohn <anikajohn@protonmail.com>
2025-04-16 10:56:09 +0200

    publish code for manuscript submission

--- readingFunctions.R
+++ readingFunctions.R
@@ -11,36 +11,36 @@ library(seqinr)
 #' @export
 #'
 #' @examples
+
 read_vcf <- function(file_path){
   
-  dt = fread(file_path, 
-             skip = grep("^#", readLines(file_path), value = TRUE)[length(grep("^#", readLines(file_path)))])
+  #if vcf has no lines w/o # == Empty
+  l = length(grep("^[^#]", readLines(file_path)))
   
-  #header has 15 lines, if 15 or less, vcf is empty
-  if(nrow(dt) <= 15){
+  if(l==0){
     
     return(NULL)
     
-  }else{
+    }else{
+    
+    dt = fread(file_path)
   
-  #different output format with parallelized lofreq
-
-  #dt[, c('DP', 'AF', 'SB', 'DP4', 'V13', 'V14') := tstrsplit(INFO, ';', fixed = TRUE)]
-  dt[, c('DP', 'AF', 'SB') := tstrsplit(INFO, ';', fixed = TRUE)[1:3]]
-  #dt[, c('DP4', 'V13', 'V14','INFO') := NULL]
-  dt[, c('INFO') := NULL]
-  dt[, `:=`(
-    AF = str_remove_all(AF, "AF="),
-    DP = str_remove_all(DP, "DP="),
-    SB = str_remove_all(SB, "SB=")
-  )]
-  
-  # Convert columns to numeric
-  dt[, c('POS', 'QUAL', 'DP', 'AF', 'SB') := lapply(.SD, as.numeric), 
-     .SDcols = c('POS', 'QUAL', 'DP', 'AF', 'SB')]
-  
-  return(dt)
-  }
+    #different output format with parallelized lofre
+    dt[, c('DP', 'AF', 'SB') := tstrsplit(INFO, ';', fixed = TRUE)[1:3]]
+    dt[, c('INFO') := NULL]
+    dt[, `:=`(
+      AF = str_remove_all(AF, "AF="),
+      DP = str_remove_all(DP, "DP="),
+      SB = str_remove_all(SB, "SB=")
+    )]
+    
+    # Convert columns to numeric
+    dt[, c('POS', 'QUAL', 'DP', 'AF', 'SB') := lapply(.SD, as.numeric), 
+       .SDcols = c('POS', 'QUAL', 'DP', 'AF', 'SB')]
+    
+    return(dt)
+    
+    }
 }
 
 
@@ -65,17 +65,23 @@ read_and_mark_vcf <- function(file_path, regex_sample = "[A-Z]\\d{1}_\\d{2}_\\d{
     
   }else{
     
-  
-    
     dt[, sample_name := str_extract(file_path,regex_sample)]
     #only care about point mutations
     dt = dt[nchar(REF) == 1 & nchar(ALT) == 1]
-    dt[, c('cell','location_code', 'year','month','day') := tstrsplit(sample_name, "_", fixed=TRUE)]
-    dt[, c('date') := paste(year,month,day,sep = "-")]
-    dt[, c('year','month','day') := NULL]
-  
-  return(dt)
     
+    if(regex_sample == "[A-Z]\\d{1}_\\d{2}_\\d{4}_\\d{2}_\\d{2}"){
+      
+      dt[, c('cell','location_code', 'year','month','day') := tstrsplit(sample_name, "_", fixed=TRUE)]
+      dt[, c('date') := paste(year,month,day,sep = "-")]
+      dt[, c('year','month','day') := NULL]
+      
+      return(dt)
+      
+    }else{
+      
+      return(dt)
+    }
+ 
   }
 }
 
